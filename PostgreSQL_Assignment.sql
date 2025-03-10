2c0ee770302f0275b5ca44e5b4902ec82f784885
Raju Ahmmed <ahraju2003@gmail.com>
2025-03-10 17:46:21 +0600

    create three table

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,1 +1,22 @@
 -- Active: 1740560784691@@127.0.0.1@5432@university_db
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    frontend_mark INT,
+    backend_mark INT,
+    status VARCHAR(50)
+);
+
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(100) NOT NULL,
+    credits INT NOT NULL
+);
+
+CREATE TABLE enrollment (
+    enrollment_id INT PRIMARY KEY,
+    student_id INT REFERENCES "students"(student_id),
+    course_id INT REFERENCES "courses"(course_id),
+);
