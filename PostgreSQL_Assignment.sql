e5c061b2343e68d3730f898596fe8d08d262708e
Raju Ahmmed <ahraju2003@gmail.com>
2025-03-16 15:13:44 +0600

    solve query 7

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -788,7 +788,14 @@ FROM courses as c
 GROUP BY
     course_name;
 
-
 -- Query 7:
 -- Calculate and display the average age of all students.
 SELECT avg(age) AS average_age FROM students;
+
+-- Query 8:
+-- Retrieve the names of students whose email addresses contain 'example.com'
+    SELECT student_name
+    FROM students
+    WHERE email LIKE '%example.com';
+
+-- Query 9:
