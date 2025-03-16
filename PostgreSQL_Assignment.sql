d7ceaf9f4a3c6d6843670816758d6d632edff250
Raju Ahmmed <ahraju2003@gmail.com>
2025-03-16 14:47:41 +0600

    solving query 6

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -772,3 +772,6 @@ WHERE
 -- Query 5:
 -- Retrieve the names of students using a limit of 2, starting from the 3rd student.
 SELECT student_name FROM students ORDER BY student_id LIMIT 2 OFFSET 2;
+
+-- Query 6:
+-- Retrieve the course names and the number of students
